833773d9d0287367443c42dc31aad0a63bb3885a
Mahenderkar sanskruthi <151664690+mahenderkar@users.noreply.github.com>
2023-12-12 18:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+
+console.log("hello sir");
+
+
+// this comment for single line or multiple line 
+/*
+used for
+multiple oe single 
+line
+*/
+//var for integers
+let oneyear=80;
+let twoyears=85;
+let thirdyear=75;
+let fourthyear=70;
+console.log(oneyear);
+console.log(twoyears);
+console.log(thirdyear);
+console.log(fourthyear);
+
+// var for strings
+let Nameofdepartment1=("computer science engineering");
+let Nameofdepartment2=("cyber security");
+let Nameofdepartment3=("Information Technoloagy");
+
+console.log(Nameofdepartment1);
+console.log(Nameofdepartment2);
+console.log(Nameofdepartment3);
+
+//var for boolean
+
+let one=true;
+let two=false;
+
+console.log(one);
+console.log(two);
+
+
+
+// constant variable: means values cannot be changed 
+
+const number=30;
+console.log(number);
+
+/*operators are of two types
+1. Arithematic operator
+2.Assignment iperator
+*/
+
+//addition operator
+ let numberofmarks1=10;
+ let numberofmarks2=20;
+
+ console.log(numberofmarks1+numberofmarks2);
+
+ //subtraction operator
+
+ let numberofmarks3=50;
+ let numberofmarks4=20;
+ console.log(numberofmarks3-numberofmarks4);
+
+ // multiplication operation
+
+ let numberofmarks5=60;
+ let numberofmarks6=50;
+ console.log(numberofmarks5*numberofmarks6);
+
+ // division operator
+ let numberofmarks7=60;
+ let numberofmarks8=50;
+ console.log(numberofmarks7/numberofmarks8);
+
+ //modulus operator
+
+ let numberofmarks9=60;
+ let numberofmarks10=50;
+ console.log(numberofmarks9%numberofmarks10);
+
+ //double**
+ let numberofmarks11=60;
+ let numberofmarks12=50;
+ console.log(numberofmarks11**numberofmarks12);
+
+//increment and decrement
+
+//increment
+
+let number1=10
+number1++
+console.log(number1)
+
+//decrement
+
+let number2=10
+--number2
+console.log(number2)
+
+let number3=11
+number3--
+console.log(number3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
